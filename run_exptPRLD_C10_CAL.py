00ba4ec27d312a848c3f7214aa25c5644b6c6925
ZhaoweiZhu1995 <zwzhu1995@gmail.com>
2021-03-26 00:25:26 -0700

    runner

--- run_exptPRLD_C10_CAL.py
+++ run_exptPRLD_C10_CAL.py
@@ -25,12 +25,12 @@ netARCH = "resnet_cifar34"
 
 
 #-------------- customized parameters --------------#
-noise_rate = 0.6
+noise_rate = 0.2
 
-lossfunc = "crossentropy"
-# lossfunc = "crossentropy_CAL"
+# lossfunc = "crossentropy"
+lossfunc = "crossentropy_CAL"
 
-gpu_idx = "3"
+gpu_idx = "0"
 #---------------------------------------------------#
 
 
@@ -52,12 +52,12 @@ chosen_classes = [0,1,2,3,4,5,6,7,8,9]
 
 # tune alpha_list if necessary
 if lossfunc == "crossentropy":
-    alpha_list = [0.0, 2.0, 2.0, 4.0] #  for distill
-    milestones = [10, 40, 80, 300]
+    alpha_list = [0.0, 2.0, 2.0] #  for distill
+    milestones = [10, 40, 80]
     sample_weight_path = None
 elif lossfunc == "crossentropy_CAL":
-    alpha_list = [0.0, 1.0, 1.0, 2.0] # for CAL
-    milestones = [10, 40, 80, 300]
+    alpha_list = [0.0, 1.0, 1.0] # for CAL
+    milestones = [10, 40, 80]
     sample_weight_path = f'sieve_65_CE_{dataset}_{noise_rate}.pt'
 else:
     ValueError('Undefined loss functions')
